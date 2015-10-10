677fd9f925e94a3ff57f46e3cbc0df431528c14c
Witten <emilwittendorff@hotmail.com>
2015-10-10 23:32:36 +0200

    Updated

---
+++ src/com/company/GameServer.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class GameServer {
+
+    String IP;
+    int port;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public void Receive(){
+
+    }
+
+    public void Send(){
+
+    }
+}
