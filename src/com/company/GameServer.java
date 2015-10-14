96ea14a8d4ab657aa5904f4a301a5dd509f6d227
MetaJumpp <matti@jumppanen.dk>
2015-10-14 13:16:58 +0200

    //Comment

--- src/com/company/GameServer.java
+++ src/com/company/GameServer.java
@@ -3,7 +3,7 @@ package com.company;
 public class GameServer {
 
     String IP; //Comment
-    int port;
+    int port; //Comment
 
     public static void main(String[] args) {
 	// write your code here
