c6f576d576433cdd6d621427cd84a429dfa6dcfd
MetaJumpp <matti@jumppanen.dk>
2015-11-11 12:07:15 +0100

    Created variables in GameBoard

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -1,7 +1,20 @@
 package com.company;
 
 
+import Cards.InfectionCard;
+import Cards.PlayerCard;
+
 public class GameBoard {
+
+    Player[] players;
+    int difficulty;
+    PlayerCard[] playerDeck;
+    InfectionCard[] infectionDeck;
+    int researchStationLeft;
+    int blueCubesLeft;
+    int yellowCubesLeft;
+    int blackCubesLeft;
+    int redCubesLeft;
 
     GameBoard(){
 
